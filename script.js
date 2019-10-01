05b7a3e4708c489e098268b43ce1f06f2c481e86
Benjamin Leskey <linewriter1024@gmail.com>
2019-09-30 23:28:19 -0400

    Basic graph.

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ function readYAML(path, done, error) {
 	return $.get(path)
 	.done(function(data) {
 		var parsed;
+		// Attempt to load as YAML.
 		try {
 			parsed = jsyaml.load(data);
 		}
+		// Send parsing errors to the error callback.
 		catch(e) {
 			error(e.message);
 			return;
 		}
+		// If successful, then run the success callback.
 		done(parsed);
 	})
 	.fail(function(object, status, reason) {
@@ -39,16 +42,61 @@ var ydata = {};
 var promises = [];
 for(const type of ["industries", "flows", "transports", "freight"]) {
 	promises.push(readYAML("/data/" + type + ".yaml", function(data) {
-		ydata[type] = data;
+		ydata[type] = data || {};
 	}, function(status) {
 		error("Unable to load " + type + " database due to error: " + status);
 	}))
 }
 
 // Data ready.
-$.when.apply($, promises).then(function() {
-	// If everything loaded OK, begin.
-	if(!errored) {
-		console.log(ydata);
-	}
-})
+window.onload = function() {
+	$.when.apply($, promises).then(function() {
+		// If everything loaded OK, begin.
+		if(!errored) {
+			var graph = Viva.Graph.graph();
+
+			var graphics = Viva.Graph.View.svgGraphics();
+			graphics.node(function(node) {
+				return Viva.Graph.svg('text')
+						.text(node.data.text);
+			})
+
+			// Construct and finalize all flow data.
+			for(var supplier in ydata.industries) {
+				for(const freight of (ydata.industries[supplier].outputs || [])) {
+					for(var demander in ydata.industries) {
+						if(demander != supplier) {
+							if((ydata.industries[demander].inputs || []).includes(freight)) {
+								var index = supplier + ";" + freight + ";" + demander;
+								ydata.flows[index] = Object.assign({}, {
+									freight: freight,
+									transport: ydata.freight[freight].transport,
+									supplier: supplier,
+									demander: demander,
+								}, ydata.flows[index] || {});
+							}
+						}
+					}
+				}
+			}
+
+			for(var industry in ydata.industries) {
+				graph.addNode("industry:" + industry, {
+					text: ydata.industries[industry].name,
+				});
+			}
+
+			for(var flow in ydata.flows) {
+				graph.addNode("flow:" + flow, {
+					text: ydata.freight[ydata.flows[flow].freight].name + " - " + ydata.flows[flow].transport.map(transport => ydata.transports[transport].name).join(", "),
+				});
+
+				graph.addLink("industry:" + ydata.flows[flow].supplier, "flow:" + flow);
+				graph.addLink("flow:" + flow, "industry:" + ydata.flows[flow].demander);
+			}
+
+			var renderer = Viva.Graph.View.renderer(graph, {graphics: graphics});
+			renderer.run();
+		}
+	});
+};
