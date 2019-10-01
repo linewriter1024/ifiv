f4f59cac11b6c2fb56cc1d6c91e0e35085a5b47a
Benjamin Leskey <linewriter1024@gmail.com>
2019-09-30 21:06:59 -0400

    Loading script.

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
-function readYAML(url, done, error) {
+"use strict";
 
+// Read YAML from URL with callbacks.
+function readYAML(path, done, error) {
+	return $.get(path)
+	.done(function(data) {
+		var parsed;
+		try {
+			parsed = jsyaml.load(data);
+		}
+		catch(e) {
+			error(e.message);
+			return;
+		}
+		done(parsed);
+	})
+	.fail(function(object, status, reason) {
+		error(reason);
+	})
 }
+
+// Have we errored?
+var errored = false;
+var errorReason = "";
+
+// Display and handle critical error.
+function error(reason) {
+	if(!errored) {
+		errored = true;
+		errorReason = reason;
+		alert(errorReason);
+	}
+}
+
+// Container for all YAML data.
+var ydata = {};
+
+// Begin downloading all YAML data.
+var promises = [];
+for(const type of ["industries", "flows", "transports", "freight"]) {
+	promises.push(readYAML("/data/" + type + ".yaml", function(data) {
+		ydata[type] = data;
+	}, function(status) {
+		error("Unable to load " + type + " database due to error: " + status);
+	}))
+}
+
+// Data ready.
+$.when.apply($, promises).then(function() {
+	// If everything loaded OK, begin.
+	if(!errored) {
+		console.log(ydata);
+	}
+})
